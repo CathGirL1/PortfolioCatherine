e27837476e7ed006d9b1c77dc3b45c3e37096ad6
cathecita <catsonderegger@gmail.com>
2025-12-15 21:03:51 -0300

    Servidor corregido del backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-require("dotenv").config({ path: ".cd/seguridad.env" });
+require("dotenv").config({ path: "./seguridad.env" });
 
 const servidor = express();
 const PUERTO = process.env.PUERTO || 5000;
@@ -10,9 +10,9 @@ const PUERTO = process.env.PUERTO || 5000;
 servidor.use(cors());
 servidor.use(express.json());
 
-servidor.post("/enviar-mensaje", async (solocitud, respuesta) => {
+servidor.post("/enviar-mensaje", async (solicitud, respuesta) => {
   
-  const { nombre, email, mensaje } = solocitud.body;
+  const { nombre, email, mensaje } = solicitud.body;
 
     const conexionCorreo = nodemailer.createTransport({
       service: "gmail",
@@ -24,8 +24,9 @@ servidor.post("/enviar-mensaje", async (solocitud, respuesta) => {
 });
 
   const opcionesCorreo = {
-    from: email,
+    from: `"Contacto Portfolio" <${process.env.GMAIL_USER}>`,
     to: process.env.GMAIL_USER,
+    replyTo: email,
     subject: "Nuevo mensaje de: " + nombre,
     text: mensaje
   };
@@ -43,3 +44,5 @@ servidor.post("/enviar-mensaje", async (solocitud, respuesta) => {
 servidor.listen(PUERTO, () => {
   console.log(`Servidor corriendo en el puerto ${PUERTO}`);
 });
+
+
